e7501b5a05cbf8d019df3abbe09ed8e6f16c7453
Declan O'Regan <declan.oregan@imperial.ac.uk>
2018-04-23 17:03:48 +0100

    Add files via upload

---
+++ Box-line-and-dot-plot.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+
+# This is the RBF and BAT data for the 6 GTN subjects MEAN PERFUSION (CORTEX) AVERAGED L-R and MEAN BAT (CORTEX) - AVERAGED L-R
+# from GTN - CORTEX AND MEDULLA - BY IMAGE EROSION - CONTROL-IMAGE T1 MAPS - ALL STUDIES
+
+# Read the data in from a separate file in long format
+
+
+RBF = read_csv("./RBF.csv")
+RBF = data.frame(RBF)
+
+
+# Create a nice theme
+
+theme_update(axis.line.y = element_line(colour="black"),
+             axis.line.x = element_line(colour="black"),
+             axis.text = element_text(colour="black"),
+             panel.grid.major = element_blank(),
+             panel.grid.minor = element_blank(),
+             panel.background = element_blank(),
+             axis.text.x  = element_text(size=20),
+             axis.text.y  = element_text(size=20),
+             axis.title.x  = element_text(size=22,vjust=1, face="bold"),
+             axis.title.y  = element_text(size=22, face = "bold", vjust=1, angle = 90),
+             axis.line = element_line(size = 1.2, linetype = "solid"),
+             axis.ticks = element_line(size = 1), legend.position="none")
+
+
+# Dot and box plot with joined lines
+
+ggplot(data=RBF, aes (x=factor(Time), y= RBF)) +
+  geom_point(size = 4, colour = "lightskyblue1") +
+  geom_line(aes(group=ID, colour="black"), alpha=I(0.3), lwd=1, linetype="dashed") +
+  scale_y_continuous (limits=c(170,260), expand=c(0,0.2)) +
+  xlab("\nTime [mins]") +
+  ylab("Cortical Perfusion [ml/min/100 g]\n") +
+  geom_boxplot(outlier.shape=NA, fill= NA,  colour="black", width=0.2, lwd=1)
+
+ggplot(data=RBF, aes (x=factor(Time), y= BAT)) +
+  geom_point(size = 4, colour = "lightskyblue1") +
+  geom_line(aes(group=ID, colour="black"), alpha=I(0.3), lwd=1, linetype="dashed") +
+  scale_y_continuous (limits=c(200,650), expand=c(0,0.2)) +
+  xlab("\nTime [mins]") +
+  ylab("Blood Arrival Time [ms]\n") +
+  geom_boxplot(outlier.shape=NA, fill= NA,  colour="black", width=0.2, lwd=1)
+
