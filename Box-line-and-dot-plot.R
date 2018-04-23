746fbceccbe8c73e420d750c9753c656075c4d22
Declan O'Regan <declan.oregan@imperial.ac.uk>
2018-04-23 20:24:24 +0100

    Update Box-line-and-dot-plot.R

--- Box-line-and-dot-plot.R
+++ Box-line-and-dot-plot.R
@@ -14,10 +14,10 @@ theme_update(axis.line.y = element_line(colour="black"),
              panel.grid.major = element_blank(),
              panel.grid.minor = element_blank(),
              panel.background = element_blank(),
-             axis.text.x  = element_text(size=20),
-             axis.text.y  = element_text(size=20),
-             axis.title.x  = element_text(size=22,vjust=1, face="bold"),
-             axis.title.y  = element_text(size=22, face = "bold", vjust=1, angle = 90),
+             axis.text.x  = element_text(size=16),
+             axis.text.y  = element_text(size=16),
+             axis.title.x  = element_text(size=18,vjust=1, face="bold"),
+             axis.title.y  = element_text(size=18, face = "bold", vjust=1, angle = 90),
              axis.line = element_line(size = 1.2, linetype = "solid"),
              axis.ticks = element_line(size = 1), legend.position="none")
 
@@ -25,9 +25,9 @@ theme_update(axis.line.y = element_line(colour="black"),
 # Dot and box plot with joined lines
 
 ggplot(data=test, aes (x=factor(Time), y= Value)) +
-  geom_line(aes(group=ID, colour="black"), alpha=I(0.5), lwd=1.5, linetype="dashed") +
-  geom_point(size = 4, colour = "lightskyblue1") +
+  geom_line(aes(group=ID), alpha=I(0.3), colour = "grey50", lwd=1.5, linetype="dashed") +
+  geom_point(size = 4, colour = "lightskyblue2") +
   scale_y_continuous (limits=c(60,150), expand=c(0,0.2)) +
-  xlab("\nTime [mins]") +
-  ylab("Units\n") +
+  xlab("Time [mins]") +
+  ylab("Units") +
   geom_boxplot(outlier.shape=NA, fill= NA,  colour="black", width=0.2, lwd=1)
